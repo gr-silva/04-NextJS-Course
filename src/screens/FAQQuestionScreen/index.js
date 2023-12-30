13d22ffe2fee7c815341d433f5c865b8e2f775a6
Gabriel Rocha <gabriel@t2tsolucoes.com>
2023-12-30 01:00:23 -0300

    feat: add cmsService and update static data with API return

--- src/screens/FAQQuestionScreen/index.js
+++ src/screens/FAQQuestionScreen/index.js
@@ -1,35 +1,41 @@
-import Head from 'next/head';
-import { Footer } from '../../components/commons/Footer';
-import { Menu } from '../../components/commons/Menu';
-import { Box, Text, theme } from '../../theme/components';
+import Head from "next/head";
+import { Footer } from "../../components/commons/Footer";
+import { Menu } from "../../components/commons/Menu";
+import { Box, Text, theme } from "../../theme/components";
+import { cmsService } from "../../infra/cms/cmsService";
 
 export async function getStaticPaths() {
   return {
-    paths: [
-      { params: { id: 'f138c88d' } },
-      { params: { id: 'h138c88d' } },
-    ],
+    paths: [{ params: { id: "f138c88d" } }, { params: { id: "h138c88d" } }],
     fallback: false,
   };
 }
 
-export function getStaticProps({ params }) {
+export async function getStaticProps({ params }) {
   const { id } = params;
+
+  const contentQuery = `
+      query {
+        contentFaqQuestion {
+          title
+          content {
+            value
+          }
+        }
+      }
+  `;
+
+  const { data } = await cmsService({
+    query: contentQuery,
+  });
+
   return {
     props: {
       id,
-      title: 'Fake Title',
-      content: `
-        <h2>Primeiro Tópico</h2>
-        <p>paragrafo simples</p>
-        <p>outro paragrafo simples</p>
-        <ul>
-          <li>Item de lista 01</li>
-          <li>Item de lista 02</li>
-        </ul>
-      `,
-    }
-  }
+      title: data.contentFaqQuestion.title,
+      content: data.contentFaqQuestion.content,
+    },
+  };
 }
 
 export default function FAQQuestionScreen({ title, content }) {
@@ -52,23 +58,23 @@ export default function FAQQuestionScreen({ title, content }) {
       >
         <Box
           styleSheet={{
-            display: 'flex',
+            display: "flex",
             gap: theme.space.x4,
-            flexDirection: 'column',
-            width: '100%',
+            flexDirection: "column",
+            width: "100%",
             maxWidth: theme.space.xcontainer_lg,
-            marginHorizontal: 'auto',
+            marginHorizontal: "auto",
           }}
         >
           <Text tag="h1" variant="heading1">
             {title}
           </Text>
-
-          <Box dangerouslySetInnerHTML={{ __html: content }} />
+          <pre>{JSON.stringify(content, null, 4)}</pre>
+          {/* <Box dangerouslySetInnerHTML={{ __html: content }} /> */}
         </Box>
       </Box>
 
       <Footer />
     </>
-  )
+  );
 }
