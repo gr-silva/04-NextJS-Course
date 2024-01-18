e55daad9980a791882ab78d943e9124d0c82f460
Gabriel Rocha <gabriel@t2tsolucoes.com>
2024-01-17 21:42:41 -0300

    feat: add cmsSection component

---
+++ src/components/cmsSections/index.js
@@ -0,0 +1,12 @@
+import { Footer } from "../commons/Footer";
+import { Menu } from "../commons/Menu";
+import { PageHomeHeroSection } from "./PageHomeHeroSection";
+import { SEOBlock } from "./SeoBlock";
+
+export const cmsSections = {
+  // CommonSeoBlockRecord: (props) => <SEOBlock {...props} />,
+  CommonSeoBlockRecord: SEOBlock,
+  CommonMenuRecord: (props) => <Menu {...props} />,
+  PagehomeHerosectionRecord: PageHomeHeroSection,
+  CommonFooterRecord: (props) => <Footer {...props} />,
+};
